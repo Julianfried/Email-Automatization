77826fa22587feeffe8fefa7ebe099ad471eb593
Julian <105244530+Julianfried@users.noreply.github.com>
2023-05-18 21:44:03 -0300

    Update send_notifying_mail.py

--- send_notifying_mail.py
+++ send_notifying_mail.py
@@ -5,12 +5,12 @@ from email.message import EmailMessage
 
 def send_notifying_mail(mail_user: str = "", mail_password: str = "") -> None:
   msg = EmailMessage()
-  user_gmail = os.getenv(USER_GMAIL)
-  password_gmail = os.getenv(PASSWORD_GMAIL)
+  mail_user = os.getenv('USER_GMAIL')
+  mail_password = os.getenv('PASSWORD_GMAIL')
 
   # Contenido
-  msg['From']= user_gmail
-  msg['To']= user_gmail
+  msg['From']= mail_user
+  msg['To']= mail_user
   msg['Subject']= "Probando mandar mails!"
   cuerpo_del_mail = 'Este es un mail enviado con Python en la clase! =D'
   msg.set_content(cuerpo_del_mail)
@@ -20,14 +20,14 @@ def send_notifying_mail(mail_user: str = "", mail_password: str = "") -> None:
   server.starttls()
 
 
-  server.login(user_gmail, password_gmail)
+  server.login(mail_user, mail_password)
 
   # enviar
   server.send_message(msg)
   server.quit();
   
-send_notifying_mail(user_gmail, password_gmail)
+send_notifying_mail(mail_user, mail_password)
 
 if __name__ == "__main__":
-  send_notifying_mail(user_gmail, password_gmail)
+  send_notifying_mail(mail_user, mail_password)
 
